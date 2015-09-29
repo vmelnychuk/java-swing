a9f727b68e2b0b100c624d56b1395326ee71435e
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-09-29 21:43:52 +0300

    add action to component

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,15 +1,11 @@
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     private TextPanel textPanel;
     private Toolbar toolbar;
-    private JButton button;
     
     public MainFrame() {
         super("Hello World");
@@ -17,16 +13,9 @@ public class MainFrame extends JFrame {
         setLayout(new BorderLayout());
         textPanel = new TextPanel();
         toolbar = new Toolbar();
-        button = new JButton("Click me");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textPanel.appendText("Hello\n");
-            }
-        });
+        toolbar.setTextPanel(textPanel);
         add(toolbar, BorderLayout.NORTH);
         add(textPanel, BorderLayout.CENTER);
-        add(button, BorderLayout.SOUTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
--- src/Toolbar.java
+++ src/Toolbar.java
@@ -1,17 +1,34 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Toolbar extends JPanel {
+public class Toolbar extends JPanel implements ActionListener {
     private static final long serialVersionUID = 1L;
     private JButton helloButton;
     private JButton goodbyeButton;
+    private TextPanel textPanel;
     public Toolbar() {
         helloButton = new JButton("hello");
         goodbyeButton = new JButton("goodbye");
+        helloButton.addActionListener(this);
+        goodbyeButton.addActionListener(this);
         setLayout(new FlowLayout(FlowLayout.LEFT));
         add(helloButton);
         add(goodbyeButton);
+    }
+    public void setTextPanel(TextPanel textPanel) {
+        this.textPanel = textPanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        JButton clickedButton = (JButton) event.getSource();
+        if (clickedButton == helloButton) {
+            textPanel.appendText("Hello\n");
+        } else if (clickedButton == goodbyeButton) {
+            textPanel.appendText("Goodbye\n");
+        }
     }
 }
