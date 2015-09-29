104abadbf5ce06d6840342ddbab9122a497c7278
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-09-29 23:15:29 +0300

    add labels and fields to form panel

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -1,19 +1,51 @@
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class FormPanel extends JPanel {
     private static final long serialVersionUID = 1L;
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okButton;
     
     public FormPanel() {
         Dimension dimension = getPreferredSize();
         dimension.width = 250;
         setPreferredSize(dimension);
         
+        nameLabel = new JLabel("Name: ");
+        occupationLabel = new JLabel("Occupation: ");
+        nameField = new JTextField(10);
+        occupationField = new JTextField(10);
+        okButton = new JButton("OK");
+        
         Border innerBorder = BorderFactory.createTitledBorder("Add Person");
         Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
         setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+        
+        setLayout(new GridBagLayout());
+        
+        GridBagConstraints bagConstraints = new GridBagConstraints();
+        
+        bagConstraints.gridx = 0;
+        bagConstraints.gridy = 0;
+        bagConstraints.weightx = 1;
+        bagConstraints.weighty = 1;
+        bagConstraints.fill = GridBagConstraints.NONE;
+        
+        add(nameLabel, bagConstraints);
+        
+        bagConstraints.gridx = 1;
+        bagConstraints.gridy = 0;
+        add(nameField, bagConstraints);
     }
 }
